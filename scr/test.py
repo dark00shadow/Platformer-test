0ae60fb6bed2ba24def915255d54696f93a14394
dark00shadow <twistcharlievlogs@gmail.com>
2020-07-16 15:59:59 +0100

    Create test.py

---
+++ scr/test.py
@@ -0,0 +1,70 @@
+import pyglet
+from pyglet.window import key
+from RectangleCollision import collision
+
+# Window
+Window = pyglet.window.Window(caption='Platformer', width=600,height=600)
+Window.set_location(Window.screen.width//2-Window.width//2, Window.screen.height//2-Window.height//2)
+Window.set_icon(pyglet.image.load('logo.ico'))
+
+# Key handler
+KeyHandler = key.KeyStateHandler()
+Window.push_handlers(KeyHandler)
+
+# Object batch
+ObjectBatch = pyglet.graphics.Batch()
+
+# Objects
+Player = pyglet.sprite.Sprite(pyglet.image.load('32x32-dark_green.png') ,x=100,y=200, batch=ObjectBatch)
+PlayerOldPosX = 0
+Block1 = pyglet.sprite.Sprite(pyglet.image.load('32x32-white.png') ,x=100,y=100, batch=ObjectBatch)
+Block2 = pyglet.sprite.Sprite(pyglet.image.load('32x32-white.png') ,x=132,y=100, batch=ObjectBatch)
+Block3 = pyglet.sprite.Sprite(pyglet.image.load('32x32-white.png') ,x=164,y=100, batch=ObjectBatch)
+Block4 = pyglet.sprite.Sprite(pyglet.image.load('32x32-white.png') ,x=196,y=100, batch=ObjectBatch)
+Block5 = pyglet.sprite.Sprite(pyglet.image.load('32x32-white.png') ,x=168,y=196, batch=ObjectBatch)
+Block6 = pyglet.sprite.Sprite(pyglet.image.load('32x32-white.png') ,x=228,y=100, batch=ObjectBatch)
+Block7 = pyglet.sprite.Sprite(pyglet.image.load('32x32-white.png') ,x=260,y=100, batch=ObjectBatch)
+Block8 = pyglet.sprite.Sprite(pyglet.image.load('32x32-white.png') ,x=228,y=132, batch=ObjectBatch)
+
+Jump = False
+
+def BlockSolid(BlockX, BlockY):
+    global Jump
+    if collision.rectangle(Player.x,Player.y ,BlockX+5,BlockY+30 ,32,32 ,22,2):
+        Player.y += 1
+        if KeyHandler[key.W]:
+            PlayerOldPosX = Player.y
+            Jump = True
+    if collision.rectangle(Player.x,Player.y ,BlockX+5,BlockY ,32,32 ,22,2):
+        Player.y -= 2
+        Jump = False
+
+
+@Window.event()
+def on_draw():
+    # Clears the window
+    Window.clear()
+    # Draws all of the objects in the Object Batch
+    ObjectBatch.draw()
+
+def Update(dt):
+    global Jump
+    # Gravity
+    Player.y -= 1
+    if KeyHandler[key.A]: Player.x -= 1
+    if KeyHandler[key.D]: Player.x += 1
+    BlockSolid(Block1.x,Block1.y)
+    BlockSolid(Block2.x,Block2.y)
+    BlockSolid(Block3.x,Block3.y)
+    BlockSolid(Block4.x,Block4.y)
+    BlockSolid(Block5.x,Block5.y)
+    BlockSolid(Block6.x,Block6.y)
+    BlockSolid(Block7.x,Block7.y)
+    BlockSolid(Block8.x,Block8.y)
+
+    if Jump == True:
+        Player.y += 3
+        if Player.y >= PlayerOldPosX + 200: Jump = False
+
+pyglet.clock.schedule_interval(Update, 1/120)
+pyglet.app.run()
